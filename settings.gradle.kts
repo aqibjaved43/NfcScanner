0f3b9d7e7f8601967182ca94af62d0ed0e867feb
Aqib Javed <aqib.javed@techificent.com>
2024-05-22 17:56:00 +0500

    v 1.0.1

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,16 +1,16 @@
 pluginManagement {
     repositories {
-        google {
-            content {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
-            }
-        }
+        google()
         mavenCentral()
         gradlePluginPortal()
     }
+    plugins {
+        id("com.android.application") version "8.4.0"
+        id("com.android.library") version "8.4.0"
+        id("org.jetbrains.kotlin.android") version "1.8.0"
+    }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
