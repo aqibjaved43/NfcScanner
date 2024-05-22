c42f051df0bd35790d31927a7d75a7dabba60c21
Aqib Javed <aqib.javed@techificent.com>
2024-05-22 18:29:34 +0500

    v1.0.2

--- NfcScanner/build.gradle.kts
+++ NfcScanner/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("maven-publish")
 }
 
 android {
@@ -32,7 +33,15 @@ android {
         jvmTarget = "1.8"
     }
 }
-
+publishing {
+    publications {
+        register<MavenPublication>("release") {
+            afterEvaluate {
+                from(components["release"])
+            }
+        }
+    }
+}
 dependencies {
 
     implementation(libs.androidx.core.ktx)
